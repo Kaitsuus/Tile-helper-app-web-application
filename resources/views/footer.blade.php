2f1f3117f7a74ef6faed5d46051287a4c9e6ff19
Kaitsuus <kai.jukarainen@gmail.com>
2023-07-26 15:07:31 +0300

    modify footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -3,13 +3,13 @@
   <div class="footer-content">
     <div class="footer-left">
       <a href="https://kaijukarainen.com/">Kaijukarainen.com</a>
+      <div class="footer-right">
+      <a href="https://www.facebook.com/profile.php?id=100092388553380" target="_blank"><img src="{{ asset('assets/facebook(1).png') }}" alt="Facebook"></a>
+      <a href="https://www.instagram.com/kaijukarainen/" target="_blank"><img src="{{ asset('assets/instagram.png') }}" alt="Instagram"></a>
+    </div>
     </div>
     <div>
       <p>&copy; <?php echo date("Y"); ?> APU-App. All rights reserved.</p>
-    </div>
-    <div class="footer-right">
-      <a href="https://www.facebook.com/profile.php?id=100092388553380" target="_blank"><img src="{{ asset('assets/facebook(1).png') }}" alt="Facebook"></a>
-      <a href="https://www.instagram.com/kaijukarainen/" target="_blank"><img src="{{ asset('assets/instagram.png') }}" alt="Instagram"></a>
     </div>
   </div>
 </footer>
