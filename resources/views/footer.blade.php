a5c6135a97b50888664db2f623ddede17715feca
Kaitsuus <kai.jukarainen@gmail.com>
2023-07-26 15:06:14 +0300

    modify links

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -2,15 +2,14 @@
 <footer>
   <div class="footer-content">
     <div class="footer-left">
-      <a href="your_other_page_url">Kaijukarainen.com</a>
+      <a href="https://kaijukarainen.com/">Kaijukarainen.com</a>
     </div>
     <div>
       <p>&copy; <?php echo date("Y"); ?> APU-App. All rights reserved.</p>
     </div>
     <div class="footer-right">
-      <a href="link_to_facebook" target="_blank"><img src="{{ asset('assets/facebook(1).png') }}" alt="Facebook"></a>
-      <a href="link_to_tiktok" target="_blank"><img src="{{ asset('assets/tiktok.png') }}" alt="TikTok"></a>
-      <a href="link_to_instagram" target="_blank"><img src="{{ asset('assets/instagram.png') }}" alt="Instagram"></a>
+      <a href="https://www.facebook.com/profile.php?id=100092388553380" target="_blank"><img src="{{ asset('assets/facebook(1).png') }}" alt="Facebook"></a>
+      <a href="https://www.instagram.com/kaijukarainen/" target="_blank"><img src="{{ asset('assets/instagram.png') }}" alt="Instagram"></a>
     </div>
   </div>
 </footer>
